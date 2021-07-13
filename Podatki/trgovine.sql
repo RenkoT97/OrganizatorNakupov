fcf658328dc793c8548f997e28b3719ec188348a
zankastelic <zkastelic15@gmail.com>
2021-07-13 16:04:35 +0200

    podatki SQL dodani

---
+++ Podatki/trgovine.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS trgovine (
+    `ime_trgovine` VARCHAR(8) CHARACTER SET utf8,
+    `lokacija` VARCHAR(24) CHARACTER SET utf8
+);
+INSERT INTO trgovine VALUES
+    ('spar','Corfe Alley'),
+    ('spar','Highlands Cliff'),
+    ('spar','Broad Heights'),
+    ('spar','Gibson Loke'),
+    ('spar','Ellicks Close'),
+    ('mercator','Hornhatch'),
+    ('mercator','Laburnum Lane'),
+    ('mercator','Thorpe Leys'),
+    ('mercator','Waterside Boulevard'),
+    ('mercator','Millbrook Market'),
+    ('tus',' Beeches Oak'),
+    ('tus','Priory Crescent'),
+    ('hofer','Cromer Point'),
+    ('hofer','Dingle Close'),
+    ('hofer','Rydal Square'),
+    ('hofer','Beechcroft Wynd'),
+    ('lidl','Mount Pleasant Woodlands'),
+    ('lidl','Priors Bridge'),
+    ('lidl','Bull Isaf'),
+    ('lidl','Bernard Fairway');
