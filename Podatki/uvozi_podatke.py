1444d148e47cef11bd934e7a57eaaaf4b022f672
RenkoT97 <tjasa.renko@gmail.com>
2021-08-24 17:03:47 +0200

    exception error

--- Podatki/uvozi_podatke.py
+++ Podatki/uvozi_podatke.py
@@ -1,16 +1,16 @@
 import sqlite3
 
-#zdej se mormo priklopit na bazo
-
-baza_datoteka = 'vsi_izdelki.db' #al .sql  to mi mal ni jasn kaj je 
-
+baza_datoteka = 'vsi_izdelki.db'
 
 def uvoziSQL(cur, datoteka):
     with open(datoteka) as f:
-        koda = f.red()
-        cc.executescript(koda)
+        koda = f.read()
+        print(koda)
+        try:
+            cur.executescript(koda)
+        except sqlite3.OperationalError:
+            pass
 
 with sqlite3.connect(baza_datoteka) as baza:
     cur = baza.cursor()
     uvoziSQL(cur, 'vsi_izdelki.sql')
-# sam mi nč ne piše notri 
