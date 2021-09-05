c9e061ad7e9b6702936961714134a7be18e58952
zankastelic <zkastelic15@gmail.com>
2021-09-05 14:40:44 +0200

    neki spreminju

--- Podatki/uvozi_podatke.py
+++ Podatki/uvozi_podatke.py
@@ -2,17 +2,32 @@ import sqlite3
 
 baza_datoteka = 'organizator_nakupov.db'
 
+CREATE_VSI_IZDELKI = '''
+    CREATE TABLE IF NOT EXISTS vsi_izdelki (
+        id_izdelka TEXT PRIMARY KEY,
+        ime_izdelka TEXT NOT NULL,
+        ime_trgovine TEXT NOT NULL,
+        firma TEXT,
+        okus TEXT,
+        redna_cena INTEGER,
+        teza INTEGER
+    )
+'''
+        
+
 def uvoziSQL(cur, datoteka):
     with open(datoteka) as f:
         koda = f.read()
         print(koda)
         try:
             cur.executescript(koda)
-        except sqlite3.OperationalError:
-            pass
+        except sqlite3.OperationalError as error:
+            print('error')
+            print(error)
 
 with sqlite3.connect(baza_datoteka) as baza:
     cur = baza.cursor()
+    cur.execute(CREATE_VSI_IZDELKI)
     uvoziSQL(cur, 'vsi_izdelki.sql')
-    uvoziSQL(cur, 'trgovine.sql')
-    uvoziSQL(cur, 'osebe.sql')
+    #uvoziSQL(cur, 'trgovine.sql')
+    #uvoziSQL(cur, 'osebe.sql')
--- aplikacija.py
+++ aplikacija.py
@@ -1,51 +1,37 @@
 from bottle import *
 import sqlite3
+import hashlib
 
 #KONFIGURACIJA
-baza_datoteka = 'vsi_izdelki.sql'
+baza_datoteka = 'organizator_nakopov.db'
 
 #Odkomentiraj, če želiš sporočila o napakah 
 debug(True) # za izpise pri razvoju 
 
+# napakaSporocilo = None
+
+def nastaviSporocilo(sporocilo = None):
+    # global napakaSporocilo
+    staro = request.get_cookie("sporocilo", secret=skrivnost)
+    if sporocilo is None:
+        response.delete_cookie('sporocilo')
+    else:
+        response.set_cookie('sporocilo', sporocilo, path="/", secret=skrivnost)
+    return staro 
+   
+# mapa za statične vire (slike,css, ...)
+static_dir = "./static"
+
+#skrivnost =  <---- to ne vem kaj je
+
+###################################
+#### PRIJAVA
+###################################
 
 # zahtevek GET s formo
 @get('/prijava') # lahko tudi @route('/prijava')
 def prijavno_okno():
-    return """
-<html>
-<head>
-
-<link rel="StyleSheet" type="text/css" href="/static/stili.css" />
-
-</head>
-
-<body>
-
-<h1>Pozdravljeni v</h1>
-
-<img src="/static/img/logo.png" alt="logo" width="450" height="450"> 
-<form action="/prijava" method="post">
-
-<div class="prijava-container">
-    <span style="font-size:xx-large;text-decoration: underline">Uporabniško ime:</span> <input name="uime" type="text" />
-    <span style="font-size:xx-large;text-decoration: underline">Geslo:</span> <input name="geslo" type="password" />
-    <input value="Prijava" type="submit" />
-</div>
-
-</form>
-</body>
-</html>
-"""
-
-# mapa za statične vire (slike,css, ...)
-static_dir = "./static"
-
-# straženje statičnih datotek 
-@route("/static/<filename:path>")
-def static(filename): 
-    return static_file(filename, root=static_dir)
-
-
+    return template('prijava.html')
 
 # zahtevek POST
 @post('/prijava') # or @route('/prijava', method='POST')
@@ -66,10 +52,21 @@ def preveri(uime, geslo):
 def img(filepath):
     return static_file(filepath, root="static/img")
 
+###################################
+#### IZDELKI
+###################################
+
+@get('/vsi_izdelki')
+def vsi_izdelki(): 
+    cur = baza.cursor()
+    vsi_izdelki = cur.execute("SELECT id_izdelka, ime_trgovine, ime_izdelka, firma, okus, redna_cena, teza FROM vsi_izdelki")
+    return template('vsi_izdelki.html', vsi_izdelki = vsi_izdelki)
 
 
-
-
+# straženje statičnih datotek 
+@route("/static/<filename:path>")
+def static(filename): 
+    return static_file(filename, root=static_dir)
 
 
 baza = sqlite3.connect(baza_datoteka, isolation_level=None)
